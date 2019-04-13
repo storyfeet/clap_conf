dbe02ecf466b7bb1356287a98fb2015d99962207
storyfeet <thebeaky@psmail.net>
2019-04-13 11:02:14 +0100

    toml get now seems to work properly

--- src/tomlget.rs
+++ src/tomlget.rs
@@ -1,33 +1,48 @@
-
-use crate::{Getter,Filter};
+use crate::{Filter, Getter};
 use toml::Value;
 
-impl<'a> Getter<'a> for Value{
-    type Iter=std::slice::Iter<'a,Value>;
-    fn value<S:AsRef<str>>(&self,s:S,f:Filter)->Option<String>{
+fn dig<S: AsRef<str>, I: Iterator<Item = S>>(v: &Value, mut i: I) -> Option<&Value> {
+    match i.next() {
+        None => Some(v),
+        Some(s) => match v {
+            Value::Table(t) => dig(t.get(s.as_ref())?, i),
+            Value::Array(a) => dig(a.get(s.as_ref().parse::<usize>().ok()?)?, i),
+            _ => None,
+        },
+    }
+}
+
+impl<'a> Getter<'a> for Value {
+    type Iter = std::slice::Iter<'a, Value>;
+    fn value<S: AsRef<str>>(&self, s: S, f: Filter) -> Option<String> {
         if f != Filter::Conf {
             return None;
         }
-        match self {
-            Value::Table(t)=>{
-                t.get(s.as_ref()).and_then(|v|v.as_str()).map(|v|v.to_string())
-            }
-            _=>{None}
-        }
+        let r = dig(self, s.as_ref().split("."))?;
+        Some(r.as_str()?.to_string())
     }
 
-    fn values<S:AsRef<str>>(&'a self,s:S,f:Filter)->Option<Self::Iter>{
+    fn values<S: AsRef<str>>(&'a self, s: S, f: Filter) -> Option<Self::Iter> {
         if f != Filter::Conf {
             return None;
         }
-        let t = if let Value::Table(t)= self{t} else {return None};
-        let v = t.get(s.as_ref())?;
+
+        let v = dig(self, s.as_ref().split("."))?;
 
         if let Value::Array(a) = v {
             return Some(a.into_iter());
         }
         None
     }
-
 }
 
+#[cfg(test)]
+mod tomltests {
+    use super::*;
+    #[test]
+    fn test_load() {
+        let t: Value = "[a.b.c]\ncar=\"red\"".parse().unwrap();
+        let r = t.value("a.b.c.car", Filter::Conf).unwrap();
+        assert_eq!(r, "red");
+    }
+}
